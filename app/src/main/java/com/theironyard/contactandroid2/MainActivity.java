aa3f7ca1aec6637481fa480b88b626fd4f0b32a6
Steven Burris <stevenburris.jr@gmail.com>
2016-09-30 21:59:53 -0400

    Add contact and remove contact feature added;

--- app/src/main/java/com/theironyard/contactandroid2/MainActivity.java
+++ app/src/main/java/com/theironyard/contactandroid2/MainActivity.java
@@ -2,16 +2,21 @@ package com.theironyard.contactandroid2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     ListView listView;
     EditText name;
     EditText phoneNumber;
     Button onAdd;
+
+    ArrayAdapter<Contact> contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +27,35 @@ public class MainActivity extends AppCompatActivity {
         name = (EditText) findViewById(R.id.name);
         phoneNumber = (EditText) findViewById(R.id.phoneNumber);
         onAdd = (Button) findViewById(R.id.onAdd);
+        contacts = new ArrayAdapter<Contact>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(contacts);
 
+        onAdd.setOnClickListener(this);
+        listView.setOnItemClickListener(this);
+        listView.setOnItemLongClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String contactName = name.getText().toString();
+        String contactNumber = phoneNumber.getText().toString();
+        if (!contactName.isEmpty() && !contactNumber.isEmpty()) {
+            Contact contact = new Contact(contactName,contactNumber);
+            contacts.add(contact);
+            name.setText("");
+            phoneNumber.setText("");
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        Contact contact = contacts.getItem(position);
+        contacts.remove(contact);
+        return true;
+    }
 }
