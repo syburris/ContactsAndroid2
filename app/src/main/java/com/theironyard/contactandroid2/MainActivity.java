adc64dc3b1f683a5b365d462f3fba847840fbf69
Steven Burris <stevenburris.jr@gmail.com>
2016-09-30 21:25:52 -0400

    Layout seems to be good

--- app/src/main/java/com/theironyard/contactandroid2/MainActivity.java
+++ app/src/main/java/com/theironyard/contactandroid2/MainActivity.java
@@ -2,12 +2,28 @@ package com.theironyard.contactandroid2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    EditText name;
+    EditText phoneNumber;
+    Button onAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        name = (EditText) findViewById(R.id.name);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        onAdd = (Button) findViewById(R.id.onAdd);
+
+
     }
+
 }
