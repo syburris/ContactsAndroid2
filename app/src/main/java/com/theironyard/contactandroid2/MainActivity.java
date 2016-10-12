e5bbf09021ea641cd98e53408466435ab3646fbd
Steven Burris <stevenburris.jr@gmail.com>
2016-10-12 12:54:09 -0400

    Finished

--- app/src/main/java/com/theironyard/contactandroid2/MainActivity.java
+++ app/src/main/java/com/theironyard/contactandroid2/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ArrayAdapter<Contact> contacts;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
